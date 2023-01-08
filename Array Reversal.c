f8c9ccd40e829fd7b8fe9b1d16a6145beb6fedbd
tazsoft <117208931+tazsoft@users.noreply.github.com>
2023-01-09 01:18:24 +0600

    Add files via upload

---
+++ Array Reversal.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,j,k;
+    int n,m;
+    scanf("%d",&n);
+    int a[n];
+
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    
+    for(i=n-1;i>=0;i--)
+
+    {
+         printf("%d ",a[i]);
+    }
+
+    return 0;
+}
